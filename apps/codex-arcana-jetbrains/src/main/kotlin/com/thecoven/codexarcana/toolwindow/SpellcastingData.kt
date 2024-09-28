4cab4c26d0be3acda0f5db3f557c93b22990686d
Katerina Skroumpelou <sk.katherine@gmail.com>
2024-09-28 10:47:38 +0300

    fix: remove gradle for deployment

--- apps/codex-arcana-jetbrains/src/main/kotlin/com/thecoven/codexarcana/toolwindow/SpellcastingData.kt
+++
@@ -1,5 +0,0 @@
-package com.thecoven.codexarcana.toolwindow
-
-data class SpellcastingData(val ingredients: Array<String>, val incantations: Array<String>, val recipes: Array<Recipe>)
-
-data class Recipe(val name: String, val ingredients: Array<String>, val incantations: Array<String>)
